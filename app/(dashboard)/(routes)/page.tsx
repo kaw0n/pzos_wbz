9cbb2686ddd17338ea949ffabd877cd0855df0df
kaw0n <130693205+kaw0n@users.noreply.github.com>
2025-01-04 20:44:39 +0100

    initial commit and clerk setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import { User } from 'lucide-react'
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton
+      afterSwitchSessionUrl='/'
+      />
+    </div>
+    
+  )
+}
